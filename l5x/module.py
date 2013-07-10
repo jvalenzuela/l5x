43e92ad06f9869f598ee67681a76e40c452547ec
Jason Valenzuela <jvalenzuela1977@gmail.com>
2013-07-10 22:11:39 +0900

    Add interface for accessing safety network numbers.

--- l5x/module.py
+++ l5x/module.py
@@ -5,8 +5,47 @@ Objects for accessing a set of I/O modules.
 from .dom import (ElementAccess, ElementDict, AttributeDescriptor)
 
 
+class SafetyNetworkNumber(AttributeDescriptor):
+    """Descriptor class for accessing safety network numbers."""
+    def from_xml(self, value):
+        """Removes the leading radix and unused 16 most significant bits."""
+        fields = value.split('_')
+        return ''.join(fields[1:])
+
+    def to_xml(self, value):
+        """Custom converter for setting a new value.
+
+        Allows the given string to omit underscores, expands to a 32-bit
+        value, and adds a radix prefix.
+        """
+        if not isinstance(value, str):
+            raise TypeError('Safety network number must be a hex string')
+        new = value.replace('_', '')
+
+        # Ensure valid hex string.
+        try:
+            x = int(new, 16)
+        except ValueError:
+            raise ValueError('Safety network number must be a hex string')
+
+        # Generate a zero-padded string and enforce 24-bit limit.
+        padded = "{0:012X}".format(x)
+        if not len(padded) == 12:
+            raise ValueError('Value must be 24-bit, 12 hex characters')
+
+        # Add radix prefix and insert underscores for the final output string.
+        fields = ['16#0000']
+        for word in range(3):
+            start = word * 4
+            end = start + 4
+            fields.append(padded[start:end])
+        return '_'.join(fields)
+
+
 class Module(ElementAccess):
     """Accessor object for a communication module."""
+    snn = SafetyNetworkNumber('SafetyNetwork')
+
     def __init__(self, element):
         ElementAccess.__init__(self, element)
         
--- l5x/project.py
+++ l5x/project.py
@@ -11,7 +11,7 @@ without worrying about low-level XML handling.
 """
 
 from .dom import (ElementAccess, ElementDict, AttributeDescriptor)
-from .module import Module
+from .module import (Module, SafetyNetworkNumber)
 from .tag import Scope
 import xml.dom.minidom
 import xml.parsers.expat
@@ -60,6 +60,33 @@ def append_child_element(name, parent):
     return new
 
 
+class ControllerSafetyNetworkNumber(SafetyNetworkNumber):
+    """Descriptor class for accessing a controller's safety network number.
+
+    This class handles the fact that the controller's safety network number
+    is stored as an attribute of the controller's module element rather than
+    the top-level controller element. Some additional work is needed to
+    direct the superclass's interface with the correct element.
+    """
+    def __get__(self, instance, owner=None):
+        mod = self.get_ctl_module(instance)
+        return super().__get__(mod)
+
+    def __set__(self, instance, value):
+        mod = self.get_ctl_module(instance)
+        super().__set__(mod, value)
+
+    def get_ctl_module(self, instance):
+        """Generates an object to access the controller module element.
+
+        While the module's name varies, the controller module is always
+        the first child within the Modules element.
+        """
+        modules = ElementAccess(instance.get_child_element('Modules'))
+        return ElementAccess(modules.child_elements[0])
+
+
 class Controller(Scope):
     """Accessor object for the controller device."""
     comm_path = AttributeDescriptor('CommPath')
+    snn = ControllerSafetyNetworkNumber('SafetyNetwork')
