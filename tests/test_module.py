97a306c1284e294fb8ab3c07676958b2057df9be
Jason Valenzuela <jvalenzuela1977@gmail.com>
2013-07-11 22:40:26 +0900

    Safety network number unit tests.

--- tests/test_module.py
+++ tests/test_module.py
@@ -64,3 +64,53 @@ class Module(unittest.TestCase):
         module = prj.modules[cls.test_module]
         module.ports[2].address = '1.2.3.4'
         fixture.teardown(prj)
+
+
+class SafetyModule(unittest.TestCase):
+    """Tests for safety modules."""
+    test_module = 'safety_module'
+
+    def setUp(self):
+        prj = fixture.setup()
+        self.module = prj.modules[self.test_module]
+
+    def test_snn_type(self):
+        """Confirm SNN is a 12 character string."""
+        snn = self.module.snn
+        self.assertIsInstance(snn, str)
+        self.assertEqual(len(snn), 12)
+
+    def test_snn_value(self):
+        """Confirm SNN number is a valid hex value."""
+        x = int(self.module.snn, 16)
+
+    def test_invalid_snn_type(self):
+        """Confirm setting SNN to a non-string raises an exception."""
+        with self.assertRaises(TypeError):
+            self.module.snn = 0
+
+    def test_invalid_snn_value(self):
+        """Confirm setting SSN to an out-of-range value raises an exception."""
+        with self.assertRaises(ValueError):
+            self.module.snn = '1000000000000'
+
+    def test_invalid_snn_str(self):
+        """Confirm setting SNN to a non-hex value raises an exception."""
+        with self.assertRaises(ValueError):
+            self.module.snn = 'not hex'
+
+    def test_set_snn(self):
+        """Test setting SNN to a legal value."""
+        self.module.snn = '0000deadbeef'
+
+    def test_set_snn_underscore(self):
+        """Test setting SNN to a value including underscores."""
+        self.module.snn = '0000_1111_2222'
+
+    @classmethod
+    def tearDownClass(cls):
+        """Sets the module's SNN in the output project."""
+        prj = fixture.setup()
+        module = prj.modules[cls.test_module]
+        module.snn = '0123456789AB'
+        fixture.teardown(prj)
