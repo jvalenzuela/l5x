484ccd9a05999a45fd14bd01a462e3ae72d06244
Jason Valenzuela <jvalenzuela1977@gmail.com>
2017-03-01 21:01:26 -0800

    Update attribute descriptor class to use .attrib element dictionary.

--- l5x/dom.py
+++ l5x/dom.py
@@ -169,24 +169,27 @@ class AttributeDescriptor(object):
         self.read_only = read_only
 
     def __get__(self, instance, owner=None):
-        if (instance.element.hasAttribute(self.name)):
-            raw = instance.element.getAttribute(self.name)
-            return self.from_xml(raw)
-        return None
+        try:
+            raw = instance.element.attrib[self.name]
+        except KeyError:
+            value = None
+        else:
+            value = self.from_xml(raw)
+        return value
 
     def __set__(self, instance, value):
         if self.read_only is True:
             raise AttributeError('Attribute is read-only')
         new_value = self.to_xml(value)
         if new_value is not None:
-            instance.element.setAttribute(self.name, new_value)
+            instance.element.attrib[self.name] = new_value
 
         # Delete the attribute if value is None, ignoring the case if the
         # attribute didn't exist to begin with.
         else:
             try:
-                instance.element.removeAttribute(self.name)
-            except xml.dom.NotFoundErr:
+                del instance.element.attrib[self.name]
+            except KeyError:
                 pass
 
     def from_xml(self, value):
--- tests/test_dom.py
+++ tests/test_dom.py
@@ -39,3 +39,73 @@ class ElementDictChild(object):
     """Dummy child access object for ElementDict testing."""
     def __init__(self, element):
         self.element = element
+
+
+class TestAttributeDescriptor(unittest.TestCase):
+    """Tests for the AttributeDescriptor class."""
+    def setUp(self):
+        self.root = etree.Element('root')
+        self.desc = AttributeDescriptorFixture(self.root)
+
+    def test_get(self):
+        """Confirm the attribute's value is returned from a read access."""
+        self.root.attrib['attr'] = 'foo'
+        self.assertEqual(self.desc.rw, 'foo')
+
+    def test_get_missing(self):
+        """Confirm None is returned when reading a nonexistent attribute."""
+        self.assertIsNone(self.desc.rw)
+
+    def test_get_conversion(self):
+        """Confirm descriptor subclasses convert read values."""
+        self.root.attrib['attr'] = '42'
+        self.assertEqual(self.desc.convert, 42)
+
+    def test_set(self):
+        """Confirm writing an attribute's value."""
+        self.desc.rw = 'foo'
+        self.assertEqual(self.root.attrib['attr'], 'foo')
+
+    def test_set_read_only(self):
+        """Confirm writing a read-only value raises an exception."""
+        with self.assertRaises(AttributeError):
+            self.desc.ro = 'foo'
+
+    def test_set_type(self):
+        """Confirm an exception is raised for non-string values."""
+        with self.assertRaises(TypeError):
+            self.desc.rw = 42
+
+    def test_del(self):
+        """Confirm writing a value of None deletes the attribute."""
+        self.root.attrib['attr'] = 'foo'
+        self.desc.rw = None
+        self.assertNotIn('attr', self.root.attrib)
+
+    def test_del_nonexistent(self):
+        """Confirm deleting a nonexistent attribute does nothing."""
+        self.desc.rw = None
+
+    def test_set_conversion(self):
+        """Confirm descriptor subclasses convert values on write."""
+        self.desc.convert = 'spam'
+        self.assertEqual(self.root.attrib['attr'], 'eggs')
+
+
+class ConverterAttributeDescriptor(l5x.dom.AttributeDescriptor):
+    """Dummy subclass to test read/write conversions."""
+    def from_xml(self, value):
+        return int(value)
+
+    def to_xml(self, value):
+        return 'eggs'
+
+
+class AttributeDescriptorFixture(object):
+    """Dummy object for AttributeDescriptor tests."""
+    rw = l5x.dom.AttributeDescriptor('attr')
+    ro = l5x.dom.AttributeDescriptor('attr', True)
+    convert = ConverterAttributeDescriptor('attr')
+
+    def __init__(self, element):
+        self.element = element
