964a4409dd90134288a85a6eb90c49b92dc3f497
Jason Valenzuela <jvalenzuela1977@gmail.com>
2018-09-02 09:32:45 -0300

    Add test fixture function to generate mock L5X projects.

--- tests/fixture.py
+++ tests/fixture.py
@@ -3,7 +3,9 @@ Utilities used by all test modules to ensure test output is accumulated into
 a single output file for final validation by RSLogix.
 """
 
+import io
 import l5x
+import xml.dom.minidom
 
 
 INPUT_FILE = 'tests/test.L5X'
@@ -22,3 +24,33 @@ def setup():
 def teardown(prj):
     """Called by tearDownModule to write the tests final output data."""
     prj.write(OUTPUT_FILE)
+
+
+def create_project(populate=None):
+    """
+    Generates a mock L5X project. The created document contains only an
+    empty set of elements required by the L5X Project class; the populate
+    callback is used to add whatever additional content needed by particular
+    test cases.
+    """
+    imp = xml.dom.minidom.getDOMImplementation()
+    doc = imp.createDocument(None, 'RSLogix5000Content', None)
+    root = doc.documentElement
+
+    controller = doc.createElement('Controller')
+    root.appendChild(controller)
+
+    # Create the top-level elements under the Controller.
+    for tag in ['Tags', 'Programs', 'Modules']:
+        element = doc.createElement(tag)
+        controller.appendChild(element)
+
+    # Dispatch the document to the populate callback to allow additional
+    # content to be added before serialization.
+    if populate is not None:
+        populate(doc)
+
+    # Serialize the document so it can be parsed as a simulated XML file.
+    xml_str = root.toxml('UTF-8').decode('UTF-8')
+    buf = io.StringIO(xml_str)
+    return l5x.Project(buf)
