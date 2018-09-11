9a0f4d702f8cf4aa46ca8ab1e469542563ec5fb0
Jason Valenzuela <jvalenzuela1977@gmail.com>
2018-09-11 15:23:10 -0300

    Implement CDATA<->element conversion.

--- l5x/project.py
+++ l5x/project.py
@@ -14,8 +14,22 @@ from .dom import (ElementAccess, ElementDict, AttributeDescriptor)
 from .module import (Module, SafetyNetworkNumber)
 from .tag import Scope
 import io
+import re
 import xml.dom.minidom
 import xml.parsers.expat
+
+
+# Logix uses CDATA sections to enclose certain content, such as rung
+# comments and tag descriptions. This inappropriate use of CDATA is
+# difficult to maintain when parsing and generating XML as CDATA is not
+# intended to be used as semantic markup. Without special handling only
+# possible with the DOM API, these CDATA sections are merged into the
+# surrounding text, which then does not import correctly into Logix.
+# To simplify the application and allow use of the ElementTree API,
+# CDATA sections are converted into normal elements before parsing,
+# then back to CDATA when the project is written. This is the tag name
+# used do enclose the original CDATA content.
+CDATA_TAG = 'CDATAContent'
 
 
 class InvalidFile(Exception):
@@ -25,6 +39,20 @@ class InvalidFile(Exception):
 
 class Project(ElementAccess):
     """Top-level container for an entire Logix project."""
+    # Opening and closing delimiters for CDATA sections.
+    CDATA_DELIMITER = ('<![CDATA[', ']]>')
+    CDATA_TAG_DELIMITER = ('<' + CDATA_TAG + '>', '</' + CDATA_TAG + '>')
+
+    # Characters from CDATA content that must be converted into escape
+    # sequences when the CDATA section is replaced with a normal tag.
+    cdata_escapes = [
+        # Amperstand must be first because it appears in other patterns.
+        # Handling it first keeps the search-replace algorithm simple.
+        ('&', '&amp;'),
+
+        ('<', '&lt;')
+    ]
+
     def __init__(self, filename):
         try:
             doc = xml.dom.minidom.parse(filename)
@@ -45,6 +73,52 @@ class Project(ElementAccess):
 
         mods = self.controller.get_child_element('Modules')
         self.modules = ElementDict(mods, 'Name', Module)
+
+    def replace_cdata(self, doc, to_cdata):
+        """Replaces the delimiters surrounding CDATA sections.
+
+        This is used both before parsing to convert CDATA sections into
+        a normal element, and before writing to revert the content back
+        into a CDATA section.
+        """
+        # Assign the delimiter sets based on the conversion direction.
+        if to_cdata:
+            remove_delim = self.CDATA_TAG_DELIMITER
+            insert_delim = self.CDATA_DELIMITER
+        else:
+            remove_delim = self.CDATA_DELIMITER
+            insert_delim = self.CDATA_TAG_DELIMITER
+
+        # Construct a regular expression to locate the CDATA content that
+        # needs to be replaced.
+        re_delim = [re.escape(s) for s in remove_delim]
+        exp = re.compile(re_delim[0] + '(?P<text>.*)' + re_delim[1])
+
+        # Generate a list of replacement pairs(old, new) for all
+        # CDATA sections.
+        replacements = []
+        for match in exp.finditer(doc):
+            # Replace reserved characters within the text.
+            text = match.group('text')
+            for pair in self.cdata_escapes:
+                # Set the order of symbol replacement based on the
+                # conversion direction.
+                if to_cdata:
+                    pair = list(pair)
+                    pair.reverse()
+
+                text = text.replace(*pair)
+
+            # Generate a new string containing the CDATA content.
+            new = "{0[0]}{1}{0[1]}".format(insert_delim, text)
+
+            replacements.append((match.group(), new))
+
+        # Replace all CDATA with the new strings.
+        for old, new in replacements:
+            doc = doc.replace(old, new, 1)
+
+        return doc
 
     def write(self, filename):
         """Outputs the document to a new file."""
