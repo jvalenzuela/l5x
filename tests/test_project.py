82525fee8d060c0c211fd967c08c2c1f8053bcf9
Jason Valenzuela <jvalenzuela1977@gmail.com>
2018-09-24 20:56:54 -0400

    Add unit tests for CDATA element to section conversions.

--- tests/test_project.py
+++ tests/test_project.py
@@ -7,7 +7,7 @@ import io
 import unittest
 from tests import fixture
 import xml.etree.ElementTree as ElementTree
-from xml.dom.minidom import getDOMImplementation
+import xml.dom.minidom
 
 
 class Parse(unittest.TestCase):
@@ -20,7 +20,7 @@ class Parse(unittest.TestCase):
 
     def test_invalid_root(self):
         """Ensure an exception is raised without the correct root element."""
-        imp = getDOMImplementation()
+        imp = xml.dom.minidom.getDOMImplementation()
         doc = imp.createDocument(None, "foo", None)
         buf = io.BytesIO(doc.toxml('UTF-8'))
         with self.assertRaises(l5x.InvalidFile):
@@ -83,3 +83,81 @@ class CDATARemoval(unittest.TestCase):
     def generate_cdata(self, text):
         """Creates a CDATA section."""
         return "<![CDATA[{0}]]>".format(text)
+
+
+class CDATAInsertion(unittest.TestCase):
+    """Tests for replacing CDATA elements with CDATA sections."""
+    def setUp(self):
+        """
+        Creates a dummy project. This is only needed to get a Project
+        instance to test the replace_cdata method.
+        """
+        self.project = fixture.create_project()
+
+    def test_element_to_CDATA(self):
+        """Confirm CDATA elements are converted to CDATA sections."""
+        src = '<root>' + self.generate_cdata('foo') + '</root>'
+        root = self.convert_parse(src)
+        self.assert_cdata_content(root, 'foo')
+
+    def test_multiple_elements(self):
+        """Confirm multiple CDATA elements are converted to CDATA sections."""
+        src = '<root>' + \
+              '<first>' + self.generate_cdata('foo') + '</first>' + \
+              '<second>' + self.generate_cdata('bar') + '</second>' + \
+              '</root>'
+        root = self.convert_parse(src)
+
+        for tag, text in [('first', 'foo'), ('second', 'bar')]:
+            element = root.getElementsByTagName(tag)[0]
+            self.assert_cdata_content(element, text)
+
+    def test_escape_sequences(self):
+        """Confirm escape sequences are converted to unescaped characters."""
+        src = '<root>' + \
+              self.generate_cdata('&amp;&lt;&gt;&quot;&apos;') + \
+              '</root>'
+        root = self.convert_parse(src)
+        self.assert_cdata_content(root, '&<>"\'')
+
+    def test_empty_element(self):
+        """Confirm an empty element is converted to an empty CDATA section."""
+        src = '<root>' + self.generate_cdata('') + '</root>'
+        self.assert_empty_cdata(src)
+
+    def test_self_closing_empty(self):
+        """Confirm a self-closing element yields to an empty CDATA section."""
+        src = '<root><CDATAContent/></root>'
+        self.assert_empty_cdata(src)
+
+    def convert_parse(self, src):
+        """
+        Converts string containing the source document, parses the
+        result, and returns the root element.
+        """
+        cdata = self.project.replace_cdata(src, True)
+        doc = xml.dom.minidom.parseString(cdata)
+        return doc.documentElement
+
+    def generate_cdata(src, text):
+        """Creates a CDATAContent element containing text."""
+        return "<CDATAContent>{0}</CDATAContent>".format(text)
+        
+    def assert_cdata_content(self, element, text):
+        """
+        Confirms an element contains a single CDATA section with a
+        given text content.
+        """
+        cdata_nodes = [n for n in element.childNodes
+                       if n.nodeType == n.CDATA_SECTION_NODE]
+        self.assertEqual(len(cdata_nodes), 1)
+        self.assertEqual(cdata_nodes[0].data, text)
+
+    def assert_empty_cdata(self, src):
+        """Validates CDATA conversion results in an empty CDATA section.
+
+        This uses basic string equality instead of parsing the converted
+        XML because empty CDATA sections are not preserved when parsed.
+        """
+        doc = self.project.replace_cdata(src, True)
+        self.assertEqual(doc, '<root><![CDATA[]]></root>')
