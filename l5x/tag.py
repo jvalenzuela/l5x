7ad436a9ec381e08910abd381252a9686249df5d
Jason Valenzuela <jvalenzuela1977@gmail.com>
2019-02-23 15:05:29 -0800

    Exclude bool data types from being used as an integer.

--- l5x/tag.py
+++ l5x/tag.py
@@ -326,7 +326,7 @@ class IntegerValue(object):
 
     def __set__(self, instance, value):
         """Sets a new value."""
-        if not isinstance(value, int):
+        if (not isinstance(value, int)) or isinstance(value, bool):
             raise TypeError('Value must be an integer')
         if (value < instance.value_min) or (value > instance.value_max):
             raise ValueError('Value out of range')
